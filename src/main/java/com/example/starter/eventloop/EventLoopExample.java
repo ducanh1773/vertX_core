98a1d501676ad0e2e4ae7750837fb0a294b10c03
ducanh1773 <101798869+ducanh1773@users.noreply.github.com>
2026-04-20 00:20:04 +0700

    push resource

--- src/main/java/com/example/starter/eventloop/EventLoopExample.java
+++ src/main/java/com/example/starter/eventloop/EventLoopExample.java
@@ -3,17 +3,35 @@ package com.example.starter.eventloop;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+import org.apache.logging.log4j.LogManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+import java.util.concurrent.TimeUnit;
+
 
 public class EventLoopExample extends AbstractVerticle {
 
+  private static final Logger LOG = LoggerFactory.getLogger(EventLoopExample.class);
+
   public static void main(String[] args) {
-    var vertx = Vertx.vertx();
+    var vertx = Vertx.vertx(
+      new VertxOptions()
+        .setMaxEventLoopExecuteTime(500)
+        .setMaxEventLoopExecuteTimeUnit(TimeUnit.MILLISECONDS)
+        .setBlockedThreadCheckInterval(1)
+        .setBlockedThreadCheckIntervalUnit(TimeUnit.SECONDS)
+    );
     vertx.deployVerticle(new EventLoopExample());
   }
 
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
-    throw new UnsupportedOperationException("nnnnn");
+    LOG.debug(" Start {} ", getClass().getName());
+    startPromise.complete();
+    Thread.sleep(5000);
   }
 }
 
