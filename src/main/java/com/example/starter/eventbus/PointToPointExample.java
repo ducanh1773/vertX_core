425a56eed73911339dffd703a35f13806137403d
ducanh1773 <anhductrannguyen170703@gmail.com>
2026-04-22 15:04:00 +0700

    update EventLoop and WorkerThread

---
+++ src/main/java/com/example/starter/eventbus/PointToPointExample.java
@@ -0,0 +1,41 @@
+package com.example.starter.eventbus;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PointToPointExample {
+  public static void main(String[] args) {
+    var vertx = Vertx.vertx();
+    vertx.deployVerticle(new Sender());
+    vertx.deployVerticle(new Receirver());
+  }
+
+  static class Sender extends AbstractVerticle {
+
+
+    @Override
+    public void start(Promise<Void> startPromise) throws Exception {
+      startPromise.complete();
+      vertx.setPeriodic(1000 , id ->{
+        vertx.eventBus().send(Sender.class.getName(), "Sending a message");
+      });
+
+    }
+
+  }
+
+  static class Receirver extends AbstractVerticle {
+    private static final Logger LOG = LoggerFactory.getLogger(Receirver.class);
+
+    @Override
+    public void start(Promise<Void> startPromise) throws Exception {
+      startPromise.complete();
+      vertx.eventBus().<String>consumer(Sender.class.getName() , message ->{
+        LOG.info("Receiver: {}" + message.body());
+      });
+    }
+  }
+}
