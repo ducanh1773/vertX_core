014f601deb991289f367bb99ecd138552887ea51
ducanh1773 <anhductrannguyen170703@gmail.com>
2026-04-23 14:28:12 +0700

    update mapping json

--- src/main/java/com/example/starter/JsonObjectExample/JsonObjectExample.java
+++ src/main/java/com/example/starter/JsonObjectExample/JsonObjectExample.java
@@ -1,6 +1,8 @@
 package com.example.starter.JsonObjectExample;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import org.junit.Test;
 
@@ -10,11 +12,31 @@ import static junit.framework.Assert.assertEquals;
 public class JsonObjectExample extends AbstractVerticle {
 
   @Test
-  public void JsonObjectCanBeMapper(){
+  public void JsonObjectCanBeMapper() {
     final JsonObject myJsonObject = new JsonObject();
-    myJsonObject.put("id" , 1);
-    myJsonObject.put("name" , "Alice");
-    myJsonObject.put("love_vert_x" , true);
-    assertEquals("" , myJsonObject.encode());
+    myJsonObject.put("id", 1);
+    myJsonObject.put("name", "Alice");
+    myJsonObject.put("love_vert_x", true);
+    assertEquals("", myJsonObject.encode());
+  }
+
+  @Test
+  public void JsonArrayCanBeMapper() {
+    final JsonArray myJsonArry = new JsonArray();
+    myJsonArry.add(new JsonObject().put("id", 1))
+      .add(new JsonObject().put("id", 2))
+      .add(new JsonObject().put("id", 3));
+    System.out.println(myJsonArry);
+  }
+
+  @Test
+    public void canMapObjects(){
+    Person person = new Person(1 , "name" , true);
+    JsonObject jsonObject = JsonObject.mapFrom(person);
+    assertEquals(person.getId() , jsonObject.getInteger("id"));
+    assertEquals(person.getName() , jsonObject.getString("name"));
+    assertEquals(person.isLevevertx() , jsonObject.getBoolean("levevertx").booleanValue());
+
   }
 }
+
